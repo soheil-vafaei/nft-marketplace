5ad6261fa65399619bce1660cb6a00c5655de60f
Soheil Vafaei <soheilvafaei@Soheils-MacBook-Air.local>
2022-12-25 19:02:08 +0330

    fixed bug in NFT.sol and add mpl contract

--- nft-marketplace-ufo/contracts/NFT.sol
+++ nft-marketplace-ufo/contracts/NFT.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.17;
 
 // ERC721 NFT Contract
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import '../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '../node_modules/@openzeppelin/contracts/utils/Counters.sol';
 
 contract NFT is ERC721URIStorage
 {
@@ -16,4 +16,20 @@ contract NFT is ERC721URIStorage
 
     address contractAddress;    
     
+    constructor (address marketplaceAddress) ERC721("ufo-collection","UFO")
+    {
+        contractAddress = marketplaceAddress;
+    }
+
+    function mintToken (string memory _tokenURI) public returns(uint)
+    {
+        _tokenids.increment();
+        uint256 newItemId = _tokenids.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, _tokenURI);
+        
+    }
+
+
 }
