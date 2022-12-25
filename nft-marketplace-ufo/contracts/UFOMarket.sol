a654b8a184fb1ea839e955ba4edc9609e7d8bbba
Soheil Vafaei <soheilvafaei@Soheils-MacBook-Air.local>
2022-12-25 19:43:43 +0330

    add function mintMarketItem

--- nft-marketplace-ufo/contracts/UFOMarket.sol
+++ nft-marketplace-ufo/contracts/UFOMarket.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.17;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import '../node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol';
-import '../node_modules/@openzeppelin/contracts/utils/Counters.sol';
+import "../node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
 // security against tranactions for multiple requests
 import "../node_modules/hardhat/console.sol";
 
-contract UFOMarket is ReentrancyGuard
-{
+contract UFOMarket is ReentrancyGuard {
     using Counters for Counters.Counter;
 
     // numebr of items minting, number of tranactions, token that have not been sold
@@ -22,17 +21,16 @@ contract UFOMarket is ReentrancyGuard
     // datereminder owner of the contract
     // charge a listing fee so the owner makes a commission
     address payable owner;
-    // we are deploying this to matic 
+    // we are deploying this to matic
 
     // for fees
     uint256 listingPrice = 0.045 ether;
 
-    constructor (){
+    constructor() {
         owner = payable(msg.sender);
     }
 
-    struct marketToken 
-    {
+    struct marketToken {
         uint256 itemId;
         uint256 tokenId;
         address nftContract;
@@ -42,33 +40,46 @@ contract UFOMarket is ReentrancyGuard
         bool sold;
     }
 
-    // token id to return wich market token - fetch wich one it is 
-    mapping (uint256 => marketToken) private idToMarketToken;
+    // token id to return wich market token - fetch wich one it is
+    mapping(uint256 => marketToken) private idToMarketToken;
 
     // listen event from frontend app
-    event MarketTokenMinted(uint indexed itemId, address indexed nftContract,uint256 tokenId,address seller,address owner,uint256 price,bool sold);
-    
+    event MarketTokenMinted(
+        uint indexed itemId,
+        address indexed nftContract,
+        uint256 tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
     // get the listing price
-    function getListingPrice() public view returns(uint)
-    {
+    function getListingPrice() public view returns (uint) {
         return listingPrice;
     }
 
     // tow func to intract with contract
     // 1. create a market item ot put it up for sale
     // 2. create a matket sale for buying and selleing between parties
-    function mintMarketItem(address nftContract, uint tokenId , uint price) public payable nonReentrant{
+    function mintMarketItem(
+        address nftContract,
+        uint tokenId,
+        uint price
+    ) public payable nonReentrant {
         // nonReentrant is a modifier to prevent reentry attack
 
-        require (price > 0 , "price must be at least one wei");
-        require (msg.value == listingPrice, "price must equals ot listing price");
+        require(price > 0, "price must be at least one wei");
+        require(
+            msg.value == listingPrice,
+            "price must equals ot listing price"
+        );
 
         _tokenids.increment();
         uint itemId = _tokenids.current();
 
         // putting it up for sale - bool - no owner
-        idToMarketToken[itemId] = marketToken
-        (
+        idToMarketToken[itemId] = marketToken(
             itemId,
             tokenId,
             nftContract,
@@ -78,6 +89,4 @@ contract UFOMarket is ReentrancyGuard
             false
         );
     }
-
-
 }
