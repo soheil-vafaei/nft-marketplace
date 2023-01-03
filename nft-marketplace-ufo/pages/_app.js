cc41d4dd72657b9067e12adfd3cde8567eae44a9
Soheil Vafaei <soheilvafaei@Soheils-MacBook-Air.local>
2023-01-04 03:15:00 +0330

    upd UI

--- nft-marketplace-ufo/pages/_app.js
+++ nft-marketplace-ufo/pages/_app.js
@@ -1,7 +1,30 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function UFOMarketplace({Component, pageProps})
+{
+  return(
+    <dev class='border-b p-6' style={{backgroundColor:'purple'}}>
+      <nav>
+        <p className='test-4x1 font-bold text-white'>UFO Marketplace</p>
+        <dev className='flex mt-4 justify-center'>
+          <Link id='link' href='/'>
+              Main MarketPlace
+          </Link>
+          <Link id='link' href='/mint-item'>
+            mint tokens
+          </Link>
+          <Link id='link' href='/nfts'>
+            my nfts
+          </Link>
+          <Link href='/account-dash'>
+            account dashboard
+          </Link>
+        </dev>
+      </nav>
+      <Component {...pageProps}/>
+    </dev>
+  )
 }
 
-export default MyApp
+export default UFOMarketplace
