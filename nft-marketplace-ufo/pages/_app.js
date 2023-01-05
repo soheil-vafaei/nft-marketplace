9fecf4b8ca0283e518cc3103a4ac6d8f059a0163
Soheil Vafaei <soheilvafaei@Soheils-MacBook-Air.local>
2023-01-05 17:53:10 +0330

    work on header

--- nft-marketplace-ufo/pages/_app.js
+++ nft-marketplace-ufo/pages/_app.js
@@ -1,19 +1,22 @@
 import "../styles/app.css"
 import Link from 'next/link'
+import Image from 'next/image'
+import ufoI from './ufoI.png'
 
 function UFOMarketplace({ Component, pageProps }) {
   return (
     <>
-
       <dev className="header--fix">
-        <h1 className="name--market ">UFO MARKETPLACE</h1>
-        <dev className="header--d">
-          <nav className="header--main">
-            <Link className="link--headers" href="/">MAIN MARKETPLACE</Link>
-            <Link className="link--headers" href="/mint-token">MINT TOKEN</Link>
-            <Link className="link--headers" href="/my-nfts">MY NFTs</Link>
-            <Link className="link--headers am-0" href="/account-dashboard">ACCOUNT DASHBOARD</Link>
-          </nav>
+        <dev className="header--main">
+          <Image src={ufoI} alt="me" width="64" height="64" />
+          <dev className="header--d">
+            <nav className="header--main">
+              <Link className="link--headers" href="/">MAIN MARKETPLACE</Link>
+              <Link className="link--headers" href="/mint-token">MINT TOKEN</Link>
+              <Link className="link--headers" href="/my-nfts">MY NFTs</Link>
+              <Link className="link--headers" href="/account-dashboard">ACCOUNT DASHBOARD</Link>
+            </nav>
+          </dev>
         </dev>
       </dev>
       <Component {...pageProps} />
