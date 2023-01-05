24c19a50df4cfd55a2e156d2a6a333394755e1b7
Soheil Vafaei <soheilvafaei@Soheils-MacBook-Air.local>
2023-01-05 20:38:07 +0330

    work on back end

--- nft-marketplace-ufo/pages/index.js
+++ nft-marketplace-ufo/pages/index.js
@@ -1,7 +1,11 @@
+import {ethers} from 'ethers'
+import { useEffect , useState} from 'react'
+import axios from 'axios'
+import Web3Modal from 'web3-modal' 
+
 export default function Home() {
   return (
     <dev>
-      {/* UFO MARKETPLACE */}
     </dev>
   )
 }
--- nft-marketplace-ufo/scripts/deploy.js
+++ nft-marketplace-ufo/scripts/deploy.js
@@ -1,31 +1,32 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+
+const { ethers } = require("hardhat");
+const fs = require('fs');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const nftMarket = await ethers.getContractFactory("UFOMarket");
+  const NFTMARKET = await nftMarket.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await NFTMARKET.deployed();
+  console.log("NFT-MARKET: ", NFTMARKET.address);
 
-  await lock.deployed();
+  const nft = await ethers.getContractFactory("NFT");
+  const NFT = await nft.deploy(NFTMARKET.address);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  await NFT.deployed();
+  console.log("NFT: ", NFT.address);
+
+  let config = `
+    export const nftmarketaddress = ${NFTMARKET.address};
+    export const nftdderss = ${NFT.address};`
+  
+  let data = JSON.stringify(config)
+  fs.writeFileSync('config.js', JSON.parse(data))
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
